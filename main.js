95608e650455c0812291e6842134176e04cc68d2
Jiyoung <jiyoung990602@gmail.com>
2024-04-04 10:00:00 +0900

    ✨ update  : 새로고침 방지

--- main.js
+++ main.js
@@ -2,9 +2,20 @@ const todoAdd = document.querySelector(".todoAdd");
 const input = document.querySelector("input");
 const list = document.querySelector(".list");
 
+// 새로고침했을 때 local storage 에 값이 있을 경우 리스트 불러오기
+const savedList = JSON.parse(localStorage.getItem("save"));
+if (savedList) {
+  for (let j = 0; j < savedList.length; j++) {
+    createTodo(savedList[j]);
+  }
+}
+
 // 기능 1. 공백 체크한 후 할 일 추가하기
-function createTodo() {
+function createTodo(Data) {
   let todoContents = input.value;
+  if (Data) {
+    todoContents = Data.contents;
+  }
 
   if (todoContents.trim() != "") {
     const todoItem = document.createElement("div");
@@ -38,6 +49,7 @@ function createTodo() {
     checkBox.addEventListener("click", () => {
       checkIcon.classList.toggle("done");
       item.classList.toggle("done");
+      saveItem();
     });
 
     // 기능 3. 할 일 목록에서 삭제하기
@@ -49,6 +61,7 @@ function createTodo() {
 todoAdd.addEventListener("submit", (event) => {
   event.preventDefault();
   createTodo();
+  saveItem();
   input.value = "";
 });
 
@@ -60,4 +73,23 @@ todoAdd.addEventListener("submit", (event) => {
 function deleteTodo(event) {
   const x = event.target.parentElement;
   x.remove();
+  saveItem();
+}
+
+// 기능 4. 새로 고침해도 데이터 날라가지 않게 Local Storage 에 담아주기.
+function saveItem() {
+  let saveList = [];
+  for (let i = 0; i < list.children.length; i++) {
+    let items = list.querySelectorAll(".item");
+    const todoObj = {
+      contents: items[i].textContent,
+      done: items[i].classList.contains("done"),
+    };
+    saveList.push(todoObj);
+  }
+  localStorage.setItem("save", JSON.stringify(saveList));
+}
+
+if (saveItem.length === 0) {
+  localStorage.removeItem("save");
 }
